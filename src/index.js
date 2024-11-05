ad0e7019d537abc14e8b43af9bd6947a98591701
lianganyin <liangan.yin@outlook.com>
2024-11-05 16:27:12 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  "docker.cyfmw.top": "https://registry-1.docker.io",
+  "quay.cyfmw.top": "https://quay.io",
+  "gcr.cyfmw.top": "https://gcr.io",
+  "k8s-gcr.cyfmw.top": "https://k8s.gcr.io",
+  "k8s.cyfmw.top": "https://registry.k8s.io",
+  "ghcr.cyfmw.top": "https://ghcr.io",
+  "cloudsmith.cyfmw.top" : "https://docker.cloudsmith.io",
+  "ecr.cyfmw.top": "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  "docker-staging.cyfmw.top": dockerHub,
 };
 
 function routeByHosts(host) {
